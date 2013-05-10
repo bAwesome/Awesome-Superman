e9e451cdd880f9f35c71c6158e953253bf8bf758
c0nsole <garrett2smart@gmail.com>
2013-05-10 16:23:29 -0500

    Updated Listener
    Added more events and catches into listener.

--- src/me/C0nsole/BeSuperman/BeSupermanListener.java
+++ src/me/C0nsole/BeSuperman/BeSupermanListener.java
@@ -1,25 +1,77 @@
 package me.C0nsole.BeSuperman;
+ 
+ 
+ 
 
+import java.util.List;
 
-
-import org.bukkit.entity.Entity;
+import org.bukkit.Material;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.potion.PotionEffectType;
+ 
 public class BeSupermanListener implements Listener{
-	public static BeSuperman plugin;
+        private BeSuperman plugin;
+        private List<String> supermen;
+ 
+        public BeSupermanListener(BeSuperman instance) {
+                plugin = instance;
+                supermen = plugin.getList();
+        }
+        @EventHandler
+        public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+            if (event.getEntity() instanceof LivingEntity){
+        	int health = ((Damageable) event.getEntity()).getHealth();
+            if(event.getDamager() instanceof Player){
+                Player player = (Player) event.getDamager();
 
-	public BeSupermanListener(BeSuperman instance) {
-		plugin = instance;
-	}
-	@EventHandler
-	public void onEntityDamage(EntityDamageEvent event) {
-		Entity entity = event.getEntity();
-		if(supermen.contains(player.getName())){
-			
-		}
-		
-	}
-}
+                  if(supermen.contains(event.getDamager())){
+                		  ((Damageable) event.getEntity()).setHealth(health-20);
+                		}
+                }
+                }
+        }
+       
+        @EventHandler
+        public void onPlayerQuitEvent(PlayerQuitEvent event){
+        	Player player = event.getPlayer();
+        	if(supermen.contains(player.getName())){
+        		player.setAllowFlight(false);
+                player.setFlying(false);
+                player.setMaxHealth(20);
+                Inventory inv = player.getInventory();
+                if(inv instanceof PlayerInventory){
+                        PlayerInventory pinv = (PlayerInventory) inv;
+                        pinv.setChestplate(new ItemStack(Material.AIR));
+                        pinv.setHelmet(new ItemStack(Material.AIR));
+                        pinv.setLeggings(new ItemStack(Material.AIR));
+                        pinv.setBoots(new ItemStack(Material.AIR));
+                       
+                }
+                player.removePotionEffect(PotionEffectType.WATER_BREATHING);
+                supermen.remove(player.getName());
+                }
+        	}
+        
+        @EventHandler
+        public void onPlayerDeath(PlayerDeathEvent event) {
+        	if(supermen.contains(event.getEntity())){
+        		event.getEntity().removePotionEffect(PotionEffectType.WATER_BREATHING);
+        		event.getEntity().setAllowFlight(false);
+        		event.getEntity().setFlying(false);
+        		supermen.remove(event.getEntity());
+        		event.getEntity().setMaxHealth(20);
+        	}
+        	
+        }
+  }
+               
